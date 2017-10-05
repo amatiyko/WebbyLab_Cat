d9ca6b2fb0c2b1e51022e30ca723391f9352411a
amatiyko <andrew.matiyko@gmail.com>
2017-10-05 20:29:21 +0300

    complete

--- webby_task.php
+++ webby_task.php
@@ -2,18 +2,11 @@
 class Animal {
     private $name;
 
-    /**
-     * Animal constructor.
-     * @param $name
-     */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
-    /**
-     * @return mixed
-     */
     public function getName()
     {
         return $this->name;
